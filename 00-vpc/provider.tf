077e0a79d59dbc057987d4f1bd020cf911095c20
llakshmipriyanka96 <llakshmipriyanka1996@gmail.com>
2025-11-18 12:54:26 +0530

    all

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.21.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "priyas386"
+    key    = "roboshop-dev-7"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
